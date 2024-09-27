49320d6178c4be5f4922d9ee1bf8d64fc86f037b
ТЕХНОРАЙ <@DESKTOP-E2H7NN2>
2024-09-27 19:57:05 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp26
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                {
+
+                    {
+                        // Task2
+                        double x = 1;
+                        double y = 2;
+
+                        if (x == 0 && y == 0)
+                        {
+                            Console.WriteLine("Точка знаходиться в початку координат.");
+                        }
+                        else if (x == 0)
+                        {
+                            Console.WriteLine("Точка знаходиться на осі Y.");
+                        }
+                        else if (y == 0)
+                        {
+                            Console.WriteLine("Точка знаходиться на осі X.");
+                        }
+                        else if (x > 0 && y > 0)
+                        {
+                            Console.WriteLine("Точка знаходиться в першій чверті.");
+                        }
+                        else if (x < 0 && y > 0)
+                        {
+                            Console.WriteLine("Точка знаходиться в другій чверті.");
+                        }
+                        else if (x < 0 && y < 0)
+                        {
+                            Console.WriteLine("Точка знаходиться в третій чверті.");
+                        }
+                        else if (x > 0 && y < 0)
+                        {
+                            Console.WriteLine("Точка знаходиться в четвертій чверті.");
+                        }
+                    }
+                }
+
+            }
+        }
+}
